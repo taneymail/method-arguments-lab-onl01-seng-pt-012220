90b7961e77dc8e06266669fc2ea307a270824013
T S <taneymail@gmail.com>
2020-02-09 20:24:32 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,7 +1,10 @@
 def introduction(name)
   puts "Hi, my name is #{name}."
+<<<<<<< HEAD
 end
 
 def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
+=======
+>>>>>>> 737dfa6249a3da0fa1db7a5f13926216df53b49c
 end
